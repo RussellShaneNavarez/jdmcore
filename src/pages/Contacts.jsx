a71992097a5cf357178118fcb5b2f4aa80918cb7
chrimitra <christianmitra7@gmail.com>
2024-03-08 23:47:41 +0100

    add email regex validation & clear input fields

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -2,22 +2,38 @@ import { Navbar } from "../components/Navbar";
 import { Footer } from '../components/Footer';
 import '../styles/Contacts.css';
 import emailjs from '@emailjs/browser';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 
 const Contacts = () => {
 
   const form = useRef();
+  const [userName, setUserName] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+
+  const emailValidation = () => {
+    const regEx = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+    if(regEx.test(userEmail)) {
+      setMessage('Email is valid');
+    } else if(!regEx.test(userEmail) && userEmail != '') {
+      setMessage('Email is not valid');
+    } else {
+      setMessage('')
+    }
+  }
 
   const sendEmail = (e) => {
     e.preventDefault();
-
-    emailjs
-      .sendForm('service_5aouvrr', 'template_r1722j7', form.current, {
+    emailValidation();
+    emailjs.sendForm('service_5aouvrr', 'template_r1722j7', form.current, {
         publicKey: 'K1BA2H9xojXnwq0j4',
       })
       .then(
         () => {
+          setUserName('');
+          setUserEmail('');
           console.log('SUCCESS!');
         },
         (error) => {
@@ -34,9 +50,10 @@ const Contacts = () => {
       <h2>Contact Us</h2>
       <div className="container-form">
         <form className="contact-form" ref={form} onSubmit={sendEmail}>
-          <p>Name: <input type="text" name="user_name" /> </p>
-          <p>Email: <input type="email" name="user_email" /></p>
-          <p>Message: <textarea name="message" /></p>
+          <p>Name: <input value={userName} onChange={(e) => setUserName(e.target.value)} type="text" name="user_name" required/> </p>
+          <p>Email: <input value={userEmail} onChange={(e) => setUserEmail(e.target.value)} type="email" name="user_email" required/></p>
+          <p>Message: <textarea name="message" required/></p>
+          <p className="message">{message}</p>
           <input type="submit" value="Send" />
         </form>
       </div>
