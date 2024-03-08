22c8a36a5f151eb1b97fc0f352c1f3ae24be097d
Russell Shane Navarez <russellnavarez18@gmail.com>
2024-03-09 00:58:42 +0100

    fixed login and register buttons not showing

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -13,22 +13,21 @@ const Card = ({ car, toggleFavorite, profile, userFavorites }) => {
       <img src={imgUrl} alt={`${brand} ${model}`} className="car-image" onClick={() => redirectToDetails(id)} />
       <div className="card-details">
         <div className="details">
-        <p><strong>{brand} {model}</strong> - {year}</p>
-        <p><strong>Description:</strong> {description}</p>
-        <p><strong>Max Power:</strong> {maxPower} hp</p>
-        <p><strong>Max Speed:</strong> {maxSpeed} km/h</p>
-        <p><strong>Acceleration:</strong> {acceleration} sec (0-100 km/h)</p>
-        <p><strong>Price:</strong> ${price}</p>
-        <p><strong>Story:</strong> {story}</p>
+          <p><strong>{brand} {model}</strong> - {year}</p>
+          <p><strong>Description:</strong> {description}</p>
+          <p><strong>Max Power:</strong> {maxPower} hp</p>
+          <p><strong>Max Speed:</strong> {maxSpeed} km/h</p>
+          <p><strong>Acceleration:</strong> {acceleration} sec (0-100 km/h)</p>
+          <p><strong>Price:</strong> ${price}</p>
+          <p><strong>Story:</strong> {story}</p>
         </div>
         <div className='favorite'>
-        {profile !== null && profile !== undefined && ( 
+          {profile ? ( // Check if profile exists
             <button onClick={() => toggleFavorite(id)}>
               {userFavorites.includes(id) ? 'Remove from Favorites' : 'Add to Favorites'}
             </button>
-          )}
-          {profile === null || profile === undefined && ( 
-            <div>
+          ) : (
+            <div className='loginregister'>
               <p>Please sign in or register to add favorites</p>
               <button>
                 <Link to="/login" className="button">Login</Link>
