a4e41fda3904065dce0da6dd3efaaff9319e94dd
chrimitra <christianmitra7@gmail.com>
2024-03-10 11:49:59 +0100

    add about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,11 +4,37 @@ import '../styles/About.css';
 
 const About = () => {
   return (
-    <div className="container">
+    <div className="container-about">
       <Navbar/>
-      <div className="content">
-        <h2>About Us</h2>
-        <p>Welcome to our website! We are a team of developers...</p>
+      <div className="content-about">
+        <div className="about-container">
+
+          <div className="title-container">
+            <p>Meet the team</p>
+          </div>
+
+          <div className="row-about">
+              <div className="col-img">
+                <img src={require('../assets/img/chri-img.jpg')} />
+                <div className="text-box">
+                  <h3>Christian Martin Mitra</h3>
+                  <p> Jr. Software Engineer | 21</p>
+                </div>
+                
+              </div>
+              <div className="col-img">
+                <img src={require('../assets/img/russ-img.jpg')} />
+                <div className="text-box">
+                  <h3>Russell Shane Navarez</h3>
+                  <p>Full Stack Developer | 22</p>
+                </div>
+              </div>
+          </div>
+          
+
+          <p></p>
+        </div>
+        
       </div>
       <Footer/>
     </div>
