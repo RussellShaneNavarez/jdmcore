58b776d1f5ea9d73896708323067554d6980279b
chrimitra <christianmitra7@gmail.com>
2024-03-11 23:21:33 +0100

    style changes cars page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,37 +1,65 @@
+import { useState } from 'react'; // Import useState hook
 import { Link } from 'react-router-dom';
 import '../styles/Card.css';
+import emptyHeartSvg from '../assets/img/empty-heart.svg';
+import fullHeartSvg from '../assets/img/full-heart.svg'; // Import the full heart icon
+import speedSvg from '../assets/img/speed-meter-svgrepo-com.svg';
+import powerSvg from '../assets/img/racing-speed-svgrepo-com.svg';
 
 const redirectToDetails = (objectId) => {
   window.location.href = `/details/${objectId}`;
 };
 
-const Card = ({ car, toggleFavorite, profile, userFavorites }) => {
-  const { id, brand, model, year, imgUrl } = car;
+const Card = ({ car, profile, userFavorites, toggleFavorite }) => {
+  const { id, brand, model, maxPower, maxSpeed,  year, imgUrl } = car;
+
+  // State to keep track of whether the car is favorited or not
+  const [favorited, setFavorited] = useState(userFavorites.includes(id));
+
+  // Function to toggle the favorite status of the car
+  const handleToggleFavorite = () => {
+    toggleFavorite(id); // Update the favorite status
+    setFavorited(!favorited); // Toggle the favorited state
+  };
 
   return (
     <div key={id} className="card"> 
       <img src={imgUrl} alt={`${brand} ${model}`} className="car-image" onClick={() => redirectToDetails(id)} />
       <div className="card-details">
         <div className="details">
-          <p><strong>{brand} {model}</strong> - {year}</p>
+
+          <div className="model-name">
+            <p><strong>{brand}</strong> - {model}</p> 
+          </div>
+
+          <div className="other-info">
+             <div id="box-info"><img className="icon-detail" src={speedSvg} /><p>{maxPower} CV</p></div>
+             <div id="box-info"><img className="icon-detail"  src={powerSvg} /><p>{maxSpeed}Km/h</p></div>
+            <p>{year}</p>
+          </div>
+
+          <div className='favorite'>
+            {profile ? (
+              <div className="heart-box">
+                {/* Conditional rendering of the heart icon based on the favorited state */}
+                <img onClick={handleToggleFavorite} className='heart-icon' src={favorited ? fullHeartSvg : emptyHeartSvg} alt="Heart Icon" />
+              </div>
+            ) : (
+              <div className='loginregister'>
+                <p>Please sign in or register to add favorites</p>
+                <button>
+                  <Link to="/login" className="button">Login</Link>
+                </button>
+                <button>
+                  <Link to="/register" className="button">Register</Link>
+                </button>
+              </div>
+            )}
+          </div>
+  
+         
         </div>
-        <div className='favorite'>
-          {profile ? ( // Check if profile exists
-            <button onClick={() => toggleFavorite(id)}>
-              {userFavorites.includes(id) ? 'Remove from Favorites' : 'Add to Favorites'}
-            </button>
-          ) : (
-            <div className='loginregister'>
-              <p>Please sign in or register to add favorites</p>
-              <button>
-                <Link to="/login" className="button">Login</Link>
-              </button>
-              <button>
-                <Link to="/register" className="button">Register</Link>
-              </button>
-            </div>
-          )}
-        </div>
+        
       </div>
     </div>
   );
